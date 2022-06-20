f854373aa30160a0c9f03a78f22b01c5ac6b34b1
Ben <ncor89b@gmail.com>
2022-06-20 17:16:25 +0200

    update user and post files of the models

--- models/user.js
+++ models/user.js
@@ -1,27 +1,87 @@
-'use strict';
+"use strict";
+const { Model } = require("sequelize");
+//? const { Sequelize, Model, DataTypes } = require("sequelize");
+
 const {
-  Model
-} = require('sequelize');
+  CheckEncryptedPassword,
+  // ensurePasswordIsStrongEnough,
+  addAuthentication
+  // addAuthenticationOn,
+} = require("../services/authentication");
+
+const { deleteFile } = require("../services/file-deleted");
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      User.hasMany(models.Post, { foreignKey: "userId" });
     }
-  } //: DataTypes User
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    admin: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'User',
+
+    softDestroy() {
+      return this.update({
+        deleted: true,
+        email: `deleted-user${this.id}@groupamania.com`,
+        imageUrl: null,
+        firstName: "Utilisateur",
+        lastName: "Supprimé",
+      });
+    }
+  }
+  User.init(
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+          //: méthode gestion affichage message d'erreur
+          async ensureEmailIsUnique(email) {
+            if (await User.findOne({ where: { email } }))
+              throw new Error(
+                "Un compte existe déjà avec cette adresse mail !"
+              );
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          // ensurePasswordIsStrongEnough,
+          CheckEncryptedPassword
+        },
+      },
+      imageUrl: DataTypes.STRING,
+      deleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      admin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+
+  addAuthentication(User);
+  //: addAuthenticationOn
+  User.afterUpdate(async (user) => {
+    if (user.dataValues.imageUrl !== user._previousDataValues.imageUrl) {
+      await deleteFile(user._previousDataValues.imageUrl);
+    }
   });
-  return User;
+
+  return User; 
 };
