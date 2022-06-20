de35017e547771bed437915dfc49c240e1976850
Ben <ncor89b@gmail.com>
2022-06-20 16:16:55 +0200

    test change branch "feature-data-migration"

--- models/user.js
+++ models/user.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  }
+  } //: DataTypes User
   User.init({
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
